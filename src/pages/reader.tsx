a7820e33098f1ca62c41ef65228a6bd6452889c4
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-06-08 23:58:01 -0400

    working on lazy load

--- src/pages/reader.tsx
+++ src/pages/reader.tsx
@@ -1,47 +1,97 @@
 import React, { MouseEvent, Props } from 'react';
 import { withRouter } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import MangaControl from 'components/mangacontrol';
 import 'pages/reader.css';
 
-import { fetchZip, extractZip } from 'services/archive.service';
+import { fetchZip, extractZip, ZipInfo } from 'services/archive.service';
 import queryString from 'query-string';
 
-class Reader extends React.Component<any, any> { // fix typing up
+enum ReaderMode {
+  longStrip = 1
+}
 
-  state = {
-    images: []
-  };
+interface State {
+  zipped: ZipInfo[],
+  images: (string|null)[],
+  pageCount: number,
+  readerMode: ReaderMode
+}
+
+class Reader extends React.Component<any, State> { // fix typing up
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      zipped: [],
+      images: [],
+      pageCount: 0,
+      readerMode: ReaderMode.longStrip
+    };
+  }
 
   queryManga = async () => {
     const qs = queryString.parse(this.props.location.search);
 
     const zipInfo = await fetchZip(`https://files.karatsubascans.com/${qs.mangafile}`);
 
-    const inflatedImages = [];
-    // lazy extract files as user scrolls to improve speed
-    for await (const deflated of zipInfo) {
-      const inflated = await extractZip(deflated.contents);
-      inflatedImages.push(inflated.toString('base64'));
-    }
-    this.setState({ images: inflatedImages });
+    // const inflatedImages: string[] = [];
+    // for await (const deflated of zipInfo) {
+    //   const inflated = await extractZip(deflated.contents);
+    //   inflatedImages.push(inflated.toString('base64'));
+    // }
+
+    this.setState(curState => ({
+      ...curState,
+      zipped: zipInfo,
+      images: zipInfo.map(zip => null),
+      pageCount: zipInfo.length
+    }));
   }
 
-  componentDidMount() {
-    this.queryManga();
+  loadPage = async (pageNumber: number) => {
+    /* TODO check if page is valid */
+
+    const inflated = await extractZip(this.state.zipped[pageNumber].contents);
+    const rawImg = inflated.toString('base64');
+
+    this.setState(curState => {
+      return {
+        ...curState,
+        images: this.state.images.splice(pageNumber, 1, rawImg)
+      }
+    });
+
+  }
+
+  async componentDidMount() {
+    await this.queryManga();
+    await this.loadPage(1);
+
   }
 
   render() {
     return (
       <div>
+        <Helmet>
+          <title>Karatsuba | Read</title>
+        </Helmet>
         <h1>Manga Reader Page</h1>
         <div className="mangaimage-container">
-          {this.state.images.map((image, ind) => (
-            <div className="mangaimage" key={ind}>
-              <img src={`data:image/jpeg;base64,${image}`}/>
-            </div>
-          ))}
+          {this.state.images.map((image, ind) => {
+            if (image == null) return;
+            return (
+              <div className="mangaimage" key={ind}>
+                <img src={`data:image/jpeg;base64,${image}`}/>
+              </div>
+            )
+          })}
         </div>
-        <MangaControl></MangaControl>
+        {/* <MangaControl></MangaControl> */}
+        <button className="mangacontrol-toggle">
+            A
+        </button>
       </div>
     )
   }
