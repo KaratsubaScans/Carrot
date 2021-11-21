3992ce9fdfdb0564c0a5f2b121468e9bb62ecde9
Zhehai <zhehaizhang1@gmail.com>
2021-11-21 18:03:26 -0500

    Hotkey page

--- src/components/HotKeys.tsx
+++ src/components/HotKeys.tsx
@@ -28,13 +28,27 @@ const HotKeys: React.FunctionComponent = () => {
       console.log('Key Binding settings not found.')
     }
   }
-  const [nextPage, setNextPage] = useState()
+  const [settings, setSettings] = useState(keyBindings);
 
   return (
     <div>
-
+      <div className="flex flex-row space-x-4">
+        <h1 className="flex-grow">Previous Page:</h1>
+        <h1>{settings.previousPage}</h1>
+      </div>
+      <div className="flex flex-row space-x-4">
+        <h1 className="flex-grow">Next Page:</h1>
+        <h1>{settings.nextPage}</h1>
+      </div>
+      <div className="flex flex-row space-x-4">
+        <h1 className="flex-grow">Previous Chapter:</h1>
+        <h1>{settings.previousChapter}</h1>
+      </div>
+      <div className="flex flex-row space-x-4">
+        <h1 className="flex-grow">Next Chapter:</h1>
+        <h1>{settings.nextChapter}</h1>
+      </div>
     </div>
-
   )
 }
 
